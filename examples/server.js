c15d60f8be15a036a86714ef966f04d0bf1fac1a
Casey Webb <notcaseywebb@gmail.com>
2015-12-04 23:13:11 -0500

    rm deps

--- examples/server.js
+++ examples/server.js
@@ -3,7 +3,6 @@
 const app = require('express')()
 const api = require('express')()
 const log = global.console.log
-const renderFile = require('ejs').renderFile
 const path = require('path')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
@@ -34,20 +33,9 @@ const compiler = webpack({
   devtool: 'eval-source-map'
 })
 
-api.set('views', __dirname + '/pages')
-api.engine('html', renderFile)
-api.engine('txt', renderFile)
-api.set('view engine', 'html')
-
-api.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-  next()
-})
-
-api.get('/page/:id', (req, res) => res.render(req.params.id))
-
+api.get('/page/:id', (req, res) => res.sendFile(path.resolve(__dirname, 'pages', req.params.id)))
 app.use('/api', api)
+
 app.use(webpackDevMiddleware(compiler, { noInfo: true }))
 app.get(/node_modules/, (req, res) => res.sendFile(path.resolve(__dirname, '..', req.path.substring(1))))
 app.get('*',            (req, res) => res.sendFile(path.resolve(__dirname, 'index.html')))
